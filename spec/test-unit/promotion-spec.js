4581aa2b7aaea33dd74d44975d7e70f9c70ea62d
Fan Wenbo <wbfan@thoughtworks.com>
2016-07-16 15:45:35 +0800

    promotion check item if need discount

--- spec/test-unit/promotion-spec.js
+++ spec/test-unit/promotion-spec.js
@@ -1,8 +1,21 @@
 var Promotion = require('../../src/promotion');
+var BasketItems = require('../../src/basket-items');
 
 describe('Promotion', function () {
+    var promotionData, promotion, basketItem1, basketItem2;
+    beforeEach(function() {
+        basketItem1 = new BasketItems(require('../fixtures/item000000.json'), 2);
+        basketItem2 = new BasketItems({barcode: 'NO-DISCOUNT'}, 1);
+        promotionData = require('../fixtures/promotion.json');
+        promotion = new Promotion(promotionData);
+    });
+
     it('constructor', function () {
-        var promotion = require('../fixtures/promotion.json');
-        expect(new Promotion(promotion)._promotion).toEqual(promotion);
+        expect(promotion._promotion).toEqual(promotionData);
+    });
+
+    it('detects item need discount or not', function () {
+        expect(promotion.checkItem(basketItem1)).toBeTruthy();
+        expect(promotion.checkItem(basketItem2)).toBeFalsy();
     });
 });
--- src/promotion.js
+++ src/promotion.js
@@ -2,4 +2,8 @@ function Promotion(promotion) {
     this._promotion = promotion;
 }
 
+Promotion.prototype.checkItem = function(basketItems) {
+    return this._promotion.barcodes.indexOf(basketItems.item.barcode) >= 0;
+};
+
 module.exports = Promotion;
